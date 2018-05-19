d4a56b7ae5cffaccf0a3b2cc68b6a64d0e31ae50
Leonardo <leo23fla@gmail.com>
2018-05-19 17:40:12 -0300

    Limpando arquivos

--- main.c
+++ main.c
@@ -3,62 +3,108 @@
 #include <time.h>
 #include <unistd.h>
 #include <locale.h>
+#include <string.h>
+#include <pthread.h>
+#include <windows.h>
 
-void combate();
-void acao_a(int *VP, int *VI);
-void acao_b(int *VP, int *VI);
-void acao_c(int *VP);
+struct inimigo
+	{
+		char nome[50];
+		char frasederrota[150];
+		char frasevitoria[150];
+		int vida;
+		int ataque_maximo;
+		int magia_maxima;
+	};
+void combate(struct inimigo *enemy);
+void acao_a (int *VP, int *VI);
+void acao_b (int *VP, int *VI);
+void acao_c (int *VP);
 void efetividade_acao(int v1, int vm);
+void *funcao_thread1 (void *);
 
 
 int main(int argc, char const *argv[])
 {
+	int i;
+	pthread_t thread1;
+	pthread_create(&thread1, NULL, funcao_thread1, NULL);
+
+	struct inimigo inimigo01;
+	struct inimigo inimigo02;
+	struct inimigo inimigo03;
+
+	strcpy (inimigo01.nome, "Cavaleiro Dourado");
+	strcpy (inimigo01.frasederrota, "Cavaleiro Dourado: Não é possível! Eu, o grande Cavaleiro Dourado, Fui derrotado por este mísero aprendiz de feiticeiro!!");
+	strcpy (inimigo01.frasevitoria, "Cavaleiro Dourado: Ha ha ha ha! Resistir é impossível! Você nunca derrotará um cavaleiro tão poderoso quanto eu!");
+	inimigo01.vida = 250;
+	inimigo01.ataque_maximo = 50;
+	inimigo01.magia_maxima = 70;
+
+	strcpy (inimigo02.nome, "Elfo Estrelar");
+	strcpy (inimigo02.frasederrota, "Elfo Estrelar: Você pode ter me vencido, mero feiticeiro. Mas garanto que do grande Chronos não vencerá!!");
+	strcpy (inimigo02.frasevitoria, "Elfo Estrelar: Você é fraco! Para me vencer, terá de abandonar todas as suas esperanças!");
+	inimigo02.vida = 265;
+	inimigo02.ataque_maximo = 65;
+	inimigo02.magia_maxima = 75;
+
+	strcpy (inimigo03.nome, "Chronos");
+	strcpy (inimigo03.frasederrota, "Chronos: Você superou todos os limites dos feiticeiros, este é o fim de todas as coisas...");
+	strcpy (inimigo03.frasevitoria, "Chronos: Este é o castigo dos que se vão contra a mim! Sofra, mero feiticeiro! Este é o seu fim!");
+	inimigo03.vida = 275;
+	inimigo03.ataque_maximo = 68;
+	inimigo03.magia_maxima = 80;
+
 	setlocale(LC_ALL, "Portuguese");
 	srand(time(NULL));
-	combate();
+	combate(&inimigo01);
+	printf("Elfo Estrelar: Você pode ter vencido um fracote como o Caveliro Dourado, mas de mim você não passará!\n");
+	sleep(1);
+	combate(&inimigo02);
+	("Chronos: Eu, o grade rei do Caos que envolve este mundo, seu grande criador, jamais permitirei uma mera cópia impura de um feiticeiro me vencer!\n");
+	sleep(1);
+	combate(&inimigo03);
 	return 0;
 }
 
-void combate()
+void combate(struct inimigo *enemy)
 {
 	int vidaP = 200;
-	int vidaI = 250;
 	char opcao;
 	do
 	{
-		system("cls");
-		printf("Você         Inimigo\n");
-		printf("Sua vida     vida inimigo\n");
-		printf("%d           %d\n", vidaP, vidaI);
-		printf("Escolha uma ação:\n");
-		printf("a = ataque rápido\n");
-		printf("b = magia\n");
-		printf("c = curar\n");
-		setbuf(stdin, NULL);
-		scanf("%c", &opcao);
+	system("cls");	
+	printf("Você         %s\n", enemy->nome);
+	printf("Sua vida     vida inimigo\n");
+	printf("%d           %d\n", vidaP, enemy->vida);
+	printf("Escolha uma ação:\n");
+	printf("a = ataque rápido\n");
+	printf("b = magia\n");
+	printf("c = curar\n");
+	setbuf (stdin, NULL);
+	scanf("%c", &opcao);
 
-		switch (opcao)
+		switch(opcao)
 		{
-		case 'a': acao_a(&vidaP, &vidaI); break;
-		case 'b': acao_b(&vidaP, &vidaI); break;
-		case 'c': acao_c(&vidaP); break;
+			case 'a': acao_a(&vidaP, &enemy->vida); break;
+			case 'b': acao_b(&vidaP, &enemy->vida); break;
+			case 'c': acao_c(&vidaP); break;	
 		}
-	} while (vidaP>0 && vidaI>0);
+	}while(vidaP>0 && enemy->vida > 0);
 	if (vidaP <= 0)
 	{
-		printf("Inimigo: Resistir é impossivel!\n");
+		printf("%s\n", enemy->frasevitoria);
 		sleep(1);
-		printf("Inimigo: Ha ha ha! Voce nunca poderá derrotar um cavaleiro tão poderoso quanto eu!\n");
+		exit(0);
 	}
-	if (vidaI <= 0)
+	if (enemy->vida <=0)
 	{
-		printf("Inimigo: Não é possivel!!!\n");
+		printf("%s\n", enemy->frasederrota);
 		sleep(1);
-		printf("Inimigo: Não é possivel! Logo eu, o tão glorioso cavaleiro imortal, perdi para um mísero aprendiz de feiticeiro!\n");
 	}
 }
 
-void acao_a(int *VP, int *VI)
+void acao_a (int *VP, int *VI)
 {
 	int valor_max = 50;
 	int aux = 10 + (rand() % 41);
@@ -69,13 +115,13 @@ void acao_a(int *VP, int *VI)
 	sleep(1);
 	printf("Agora é a vez do inimigo\n");
 	sleep(1);
-	*VP = *VP - auxII;
-	efetividade_acao(auxII, valor_max);
-	printf("O inimigo tirou %d pontos de vida de você\n", auxII);
-	sleep(1);
+    *VP = *VP - auxII;
+    efetividade_acao(auxII, valor_max);
+    printf("O inimigo tirou %d pontos de vida de você\n", auxII);
+    sleep(1);
 
 }
-void acao_b(int *VP, int *VI)
+void acao_b (int *VP, int *VI)
 {
 	int valor_max = 70;
 	int aux = rand() % 71;
@@ -87,12 +133,12 @@ void acao_b(int *VP, int *VI)
 	printf("Agora é a vez do inimigo\n");
 	sleep(1);
 	efetividade_acao(auxII, valor_max);
-	*VP = *VP - auxII;
-	printf("O inimigo tirou %d pontos de vida de você\n", auxII);
-	sleep(1);
+    *VP = *VP - auxII;
+    printf("O inimigo tirou %d pontos de vida de você\n", auxII);
+    sleep(1);
 
 }
-void acao_c(int *VP)
+void acao_c (int *VP)
 {
 	int valor_max = 70;
 	int aux = 10 + (rand() % 61);
@@ -110,28 +156,28 @@ void acao_c(int *VP)
 }
 void efetividade_acao(int v1, int vm)
 {
-	if (v1 <= (25 * vm) / 100)
+	if (v1 <= (25*vm)/100)
 	{
 		printf("...\n");
 		sleep(1);
 		printf("Não foi tao efetivo assim...\n");
 		sleep(1);
 	}
-	else if ((v1 >= (26 * vm) / 100) && (v1 <= (65 * vm) / 100))
+	else if ((v1 >= (26*vm)/100) && (v1<= (65*vm)/100))
 	{
 		printf("...\n");
 		sleep(1);
 		printf("Foi bem efetivo!\n");
 		sleep(1);
 	}
-	else if ((v1 >= (66 * vm) / 100) && (v1 <= (79 * vm) / 100))
+	else if ((v1 >= (66*vm)/100) && (v1 <= (79*vm)/100))
 	{
 		printf("...\n");
 		sleep(1);
 		printf("Foi muito efetivo!\n");
 		sleep(1);
 	}
-	else if (v1 >= (80 * vm) / 100)
+	else if (v1 >= (80*vm)/100)
 	{
 		printf("...\n");
 		sleep(1);
@@ -139,3 +185,8 @@ void efetividade_acao(int v1, int vm)
 		sleep(1);
 	}
 }
+void *funcao_thread1 (void *parametro)
+{
+PlaySound("som.wav", NULL, SND_FILENAME);
+}
+
