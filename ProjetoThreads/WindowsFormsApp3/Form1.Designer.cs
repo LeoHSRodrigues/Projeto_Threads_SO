64a3bc1c42cb338e58b8519a99123f9aafbf5ad1
leo23fla <leo23fla@gmail.com>
2018-08-12 19:07:45 -0300

    Gui - Personagens
    GUI feita em c# pra selecionar a classe de herói.

---
+++ ProjetoThreads/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Start = new System.Windows.Forms.Button();
+            this.classeA = new System.Windows.Forms.ComboBox();
+            this.nomeA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Start.Location = new System.Drawing.Point(68, 127);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(121, 34);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Começar!";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // classeA
+            // 
+            this.classeA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.classeA.FormattingEnabled = true;
+            this.classeA.Items.AddRange(new object[] {
+            "Mago",
+            "Feiticeiro",
+            "Guerreiro",
+            "Monge"});
+            this.classeA.Location = new System.Drawing.Point(68, 90);
+            this.classeA.Name = "classeA";
+            this.classeA.Size = new System.Drawing.Size(121, 21);
+            this.classeA.TabIndex = 2;
+            // 
+            // nomeA
+            // 
+            this.nomeA.Location = new System.Drawing.Point(68, 64);
+            this.nomeA.Name = "nomeA";
+            this.nomeA.Size = new System.Drawing.Size(121, 20);
+            this.nomeA.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Classe";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bem Vindo Aventureiro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 173);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nomeA);
+            this.Controls.Add(this.classeA);
+            this.Controls.Add(this.Start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RPG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ComboBox classeA;
+        private System.Windows.Forms.TextBox nomeA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
